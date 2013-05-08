e0dfda4f1c32d79be648650118d8eca0c5f3dc4d
k4nagatsuki <none@none>
2013-05-08 22:54:08 +0900

    フレームを飛ばす仕組み(未使用)

--- cw/effectbooster.py
+++ cw/effectbooster.py
@@ -146,7 +146,7 @@ class _JpySubImage(cw.image.Image):
 
             self.cache.save_position(pos)
 
-    def _drawtemp_impl(self, background, pos):
+    def _drawtemp_impl(self, background, pos, redraw=True):
         """backgroundのposの位置に一時描画。"""
         image = self.get_image()
         image = self.clip_tempimg(image, pos)
@@ -155,15 +155,21 @@ class _JpySubImage(cw.image.Image):
         rect = rect.clip(background.get_rect())
 
         if 0 < rect[2] and 0 < rect[3]:
-            if not self.animation == 1:
-                before = background.subsurface(rect).copy()
+            if redraw:
+                if not self.animation == 1:
+                    before = background.subsurface(rect).copy()
 
-            background.blit(image, pos, special_flags=self.paintmode)
-            cw.cwpy.draw()
+                background.blit(image, pos, special_flags=self.paintmode)
+                cw.cwpy.draw()
+
+                if not self.animation == 1:
+                    background.blit(before, rect.topleft)
+            else:
+                if self.animation == 1:
+                    background.blit(image, pos, special_flags=self.paintmode)
+                    cw.cwpy.draw()
+
             self.wait()
-
-            if not self.animation == 1:
-                background.blit(before, rect.topleft)
 
     def clip_tempimg(self, image, pos):
         if self.animeclip:
