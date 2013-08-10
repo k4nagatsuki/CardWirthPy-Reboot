4ac617a132dbafaee4868fded91b76898cce15f3
k4nagatsuki <none@none>
2013-08-10 17:05:30 +0900

    メニューカードのロック中に方向キーやホイールでカードを選択できる

--- cw/eventhandler.py
+++ cw/eventhandler.py
@@ -96,7 +96,7 @@ class EventHandler(object):
         """
         方向キーイベント。カードのフォーカスを変更する。
         """
-        if cw.cwpy.is_runningevent():
+        if cw.cwpy.is_runningevent() or cw.cwpy.lock_menucards:
             return
 
         cw.cwpy.has_inputevent = True
