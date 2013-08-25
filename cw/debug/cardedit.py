4628f3fe6e86840a9a06d03e94d7d3dcbd0068c5
k4nagatsuki <none@none>
2013-08-25 14:16:21 +0900

    デバッガからカードを配付した時の素材参照の書き換えが配付前のデータに影響する

--- cw/debug/cardedit.py
+++ cw/debug/cardedit.py
@@ -242,7 +242,7 @@ class CardEditDialog(wx.Dialog):
             if index <= -1:
                 break
             notscenariocard = not cw.cwpy.is_playingscenario()
-            data = self.datalist[index]
+            data = copy.deepcopy(self.datalist[index])
             header = self.list[index]
             cw.content.get_card(data, target, notscenariocard=notscenariocard, copymaterialfrom=header.scedir, fromdebugger=True)
             count += 1
