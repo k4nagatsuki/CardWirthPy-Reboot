7911d5a5cea079e0f9f0748d5cdd6e647c3e29a8
takuto_cw <shell@leafboat.sakura.ne.jp>
2013-08-28 20:46:29 +0900

    特定環境下で、ボタンの Enabled・Disabled が見分けづらい

--- cw/imageretouch.py
+++ cw/imageretouch.py
@@ -671,6 +671,56 @@ def _blend_sub_1_50(dest, source):
     buf = "".join(buf)
     return pygame.image.frombuffer(buf, (w, h), "RGBA").convert_alpha()
 
+def to_disabledimage(wxbmp):
+    """
+    通常時のボタン画像からdisabled用の画像を作る
+    グレイスケール処理後、RGB値の範囲を 0～255 から min～max に変更
+    wxbmp: wx.Bitmap
+    """
+    # 最終的なRGB値の範囲を設定
+    min, max = 140, 240
+    # 右下の照り返し色
+    light = (255, 255, 255)
+
+    image = cw.image.conv2surface(wxbmp)
+    image = to_grayscale(image)
+    colorkey = image.get_colorkey()[0:3]
+
+    pxarray = pygame.PixelArray(image)
+    lastseq = []
+
+    # 上・左上・左の３ピクセルが全部、透過色もしくは照り返し色なら
+    # 透過色に。そうでないなら照り返し色に。
+    def check_reflection(neighbor_pixels):
+        for pixel in neighbor_pixels:
+            if not (pixel == colorkey or pixel == light):
+                return light
+
+        return colorkey
+
+    for x, pxs in enumerate(pxarray):
+        seq = []
+
+        for px in pxs:
+            (rgb, ) = hex2color(px)[0:1]
+
+            if (rgb, ) == colorkey[0:1]:
+                if len(seq) == 0 or len(lastseq) == 0:
+                    seq.append(colorkey)
+                else:
+                    L = len(seq)
+                    color = check_reflection((seq[L-1], lastseq[L-1], lastseq[L]))
+                    seq.append(color)
+            else:
+                rgb = rgb * (max - min) / 255  + min
+                seq.append((rgb, rgb, rgb))
+
+        pxarray[x] = seq
+        lastseq = []
+        lastseq = seq
+
+    return cw.image.conv2wxbmp(image)
+
 def hex2color(hexnum):
     """RGBデータの16進数を(r, g, b)のタプルで返す。
     hexnum: 16進数。
