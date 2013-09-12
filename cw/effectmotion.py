740c4d4dce0a4e51bf5c8c12ec4ab3bea56fd34c
tachi_gigas <tachi_gigas@yahoo.co.jp>
2013-09-12 22:58:25 +0900

    e8db143 8e98ac3 考慮漏れ

--- cw/effectmotion.py
+++ cw/effectmotion.py
@@ -243,7 +243,8 @@ class Effect(object):
             self.animate(target, True)
 
         # 吸収効果があったら、使用者のカードを回転させて更新する。
-        if self.user and self.count_motion("absorb"):
+        if self.user and self.count_motion("absorb")\
+                     and self.user.is_injured():
             cw.cwpy.play_sound(u"効果（呪縛）.ogg")
             cw.animation.animate_sprite(self.user, "hide")
             self.user.update_image()
