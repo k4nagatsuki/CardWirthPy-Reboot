17f09c8baec5c203fd670490316ce990798ca480
k4nagatsuki <none@none>
2013-09-14 09:19:19 +0900

    lock_menucards解除の遅延は今は不要

--- cw/frame.py
+++ cw/frame.py
@@ -598,10 +598,7 @@ class Frame(wx.Frame):
         cw.cwpy.mousepos = (-1, -1)
 
         cw.cwpy._showingdlg -= 1
-        if cw.cwpy._showingdlg <= 0:
-            def func():
-                cw.cwpy.lock_menucards = False
-            cw.cwpy.exec_func(func)
+        cw.cwpy.lock_menucards = False
 
     def change_selection(self, selection):
         """選択カードを変更し、色反転させる。
