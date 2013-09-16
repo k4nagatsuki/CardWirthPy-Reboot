b871eb388d6a13f7a36d2161196b043c4891fea7
k4nagatsuki <none@none>
2013-09-16 20:18:17 +0900

    is_runningevent()の変更の影響でF9時にエラーが出る

--- cw/frame.py
+++ cw/frame.py
@@ -529,7 +529,7 @@ class Frame(wx.Frame):
             else:
                 def stop():
                     cw.cwpy.exec_func(cw.cwpy.sdata.f9)
-                    if cw.cwpy.is_runningevent():
+                    if cw.cwpy.is_runningevent() and cw.cwpy.event.get_event():
                         # イベント中断
                         cw.cwpy.event.get_event().exit_func = cw.cwpy.sdata.f9
                         raise cw.event.EffectBreakError()
