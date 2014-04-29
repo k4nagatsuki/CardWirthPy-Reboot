efcf4de1c92f6bf7967a66229ace30fdfd85fa1d
k4nagatsuki <none@none>
2014-04-29 17:43:20 +0900

    開始エリアの設定されていないシナリオが強制終了した後、そのシナリオがプレイ中の状態になってしまう

--- cw/thread.py
+++ cw/thread.py
@@ -925,6 +925,7 @@ class CWPy(_Singleton, threading.Thread):
                 if not self.sdata.startid in self.sdata.areas:
                     # 開始エリアが存在しない(帰還)
                     self.check_level(True)
+                    self.sdata.end()
                     self.set_yado()
                 elif musicpath is None or\
                                 self.music.path == self.music.get_path(musicpath):
