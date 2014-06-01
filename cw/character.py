35198adb88771486681478802da0512e672b4baa
k4nagatsuki <none@none>
2014-06-01 21:00:54 +0900

    中毒は他パラメータとは別に画像を更新する

--- cw/character.py
+++ cw/character.py
@@ -1817,7 +1817,7 @@ class Character(object):
 
                 if self.status <> "reversed" and self.status <> "hidden":
                     cw.animation.animate_sprite(self, "lateralvibe")
-                updateimage = True
+                self.update_image()
 
         # 麻痺
         if self.is_paralyze() and not self.is_petrified():
@@ -1927,6 +1927,10 @@ class Character(object):
         if self.is_unconscious():
             self.set_unconsciousstatus()
 
+        # 敵が中毒効果で死亡していたら、死亡イベント開始
+        if isinstance(self, Enemy) and self.is_dead() and oldalive:
+            self.events.start(1)
+
         # 画像更新
         if flag or updateimage:
             if self.status <> "reversed" and self.status <> "hidden":
@@ -1938,10 +1942,6 @@ class Character(object):
                     self.update_image()
             else:
                 self.update_image()
-
-        # 敵が中毒効果で死亡していたら、死亡イベント開始
-        if isinstance(self, Enemy) and self.is_dead() and oldalive:
-            self.events.start(1)
 
 class Player(Character):
     def lost(self):
