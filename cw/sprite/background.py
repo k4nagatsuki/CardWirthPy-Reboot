d2b5ff4f5f87749f13dece383158f4c251c5cee1
k4nagatsuki <none@none>
2014-07-11 23:18:45 +0900

    全面を覆うセルのフラグオンからオフにしての再表示でその背後の画像が表示されない

--- cw/sprite/background.py
+++ cw/sprite/background.py
@@ -286,7 +286,7 @@ class BackGround(base.CWPySprite):
 
         image, anime, update = self.load_surface(path, mask, cw.s(size), flag, doanime=doanime)
 
-        if pos == (0, 0) and size == cw.SIZE_AREA and visible and not mask:
+        if image and pos == (0, 0) and size == cw.SIZE_AREA and visible and not mask and not flag:
             # 背景を覆ったので背景継承を取り消す
             del bgs[:]
             bginhrt = False
