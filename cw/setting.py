08baa115602e991cbfd0f7e64afca1bbb47ba26a
k4nagatsuki <none@none>
2014-08-31 17:37:22 +0900

    参照属性の設定値のデフォルト値が上書きされる

--- cw/setting.py
+++ cw/setting.py
@@ -149,7 +149,11 @@ class Setting(object):
 
         for t in inspect.getmembers(self, lambda t: not inspect.isroutine(t)):
             if not t[0].startswith("__"):
-                setattr(self, "%s_init" % (t[0]), t[1])
+                if hasattr(t[1], "copy"):
+                    v = t[1].copy()
+                else:
+                    v = t[1]
+                setattr(self, "%s_init" % (t[0]), v)
 
         if not os.path.isfile("Settings.xml"):
             self.write()
