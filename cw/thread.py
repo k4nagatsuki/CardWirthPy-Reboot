91ef3a006680d3059ce455ad407568547d767e07
k4nagatsuki <none@none>
2015-06-30 20:05:18 +0900

    ステータスボタンにフォーカスを合わせた状態でダイアログを閉じると、背景の右クリック操作が一時的に効かなくなる

--- cw/thread.py
+++ cw/thread.py
@@ -1020,6 +1020,7 @@ class CWPy(_Singleton, threading.Thread):
         self._showingdlg -= 1
         if self._showingdlg <= 0:
             self.frame.app.SetCallFilterEvent(False)
+            self.exec_func(self.clear_selection)
 
     def exec_func(self, func, *args, **kwargs):
         """CWPyスレッドで指定したファンクションを実行する。
