2bd4d465aa394f3cddc764caa3bfc8cabe4189a9
k4nagatsuki <none@none>
2015-10-05 22:58:20 +0900

    全てホールド設定が消えてしまう

--- cw/character.py
+++ cw/character.py
@@ -20,14 +20,14 @@ class Character(object):
         self.name = self.data.gettext("Property/Name", "")
         # レベル
         self.level = self.data.getint("Property/Level")
-        # 各種所持カードのリスト
-        self.cardpocket = self.get_cardpocket()
         # 全てホールド
         self.hold_all = [
             self.data.getbool("SkillCards", "hold_all", False),
             self.data.getbool("ItemCards", "hold_all", False),
             self.data.getbool("BeastCards", "hold_all", False),
         ]
+        # 各種所持カードのリスト
+        self.cardpocket = self.get_cardpocket()
         # 現在ライフ・最大ライフ
         self.life = self.data.getint("Property/Life")
         self.maxlife = self.data.getint("Property/Life", "max")
