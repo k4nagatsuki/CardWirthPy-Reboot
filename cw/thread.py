87ab4c21ee8601fe86371af0eceefc6a387e4240
k4nagatsuki <none@none>
2015-10-15 20:22:02 +0900

    拠点でスキンを切り替えた時、切り替え先のスキンの拠点データが元のスキンと異なる背景情報や音声情報を持っていると完全に切り替わらない

--- cw/thread.py
+++ cw/thread.py
@@ -382,8 +382,11 @@ class CWPy(_Singleton, threading.Thread):
                     self.cardgrp.remove(sprite)
                     self.mcards.remove(sprite)
             self.sdata.change_data(self.areaid)
+            self.background.load(self.sdata.get_bgdata(), False, ("None", "None"))
             self.set_mcards(self.sdata.get_mcarddata(), False, True, False)
             self.deal_cards()
+            if not self.is_playingscenario():
+                self.sdata.start_event(keynum=1)
 
         self.clear_selection()
         if self.rsrc:
