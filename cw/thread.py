1984c6dd151f67f081c0d2b86198e1da91a5f889
k4nagatsuki <none@none>
2015-12-16 22:58:43 +0900

    隠蔽キャラクタにカーソルを合わせてデバッグモードを解除すると隠蔽キャラクタの選択が解除されない

--- cw/thread.py
+++ cw/thread.py
@@ -596,7 +596,7 @@ class CWPy(_Singleton, threading.Thread):
             self.frame.exec_func(self.frame.debugger.Close)
 
         cw.data.redraw_cards(debug)
-        self.draw()
+        self.clear_selection()
 
     def update_infocard(self):
         """デバッガ等から所有情報カードの変更を
