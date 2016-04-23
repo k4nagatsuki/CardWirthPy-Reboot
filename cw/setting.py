494935347684c6fe2f27affa107a7e847fd715e7
k4nagatsuki <none@none>
2016-04-23 21:28:22 +0900

    スクリーンショットのファイルパスのデフォルト値を`ScreenShot/<宿名>/パーティ名_<日時>_in_<シナリオ名>.png`に変更

--- cw/setting.py
+++ cw/setting.py
@@ -333,8 +333,8 @@ class Setting(object):
         self.scenario_narrowtype = 0
         self.scenario_sorttype = 0
         self.ssinfoformat = u"[%scenario%[(%author%)] - ][%party% at ]%yado%"
-        self.ssfnameformat = u"ScreenShot/%year%%month%%day%_%hour%%minute%%second%_%millisecond%000.png"
-        self.cardssfnameformat = u"ScreenShot/%party%_%year%%month%%day%_%hour%%minute%%second%_%millisecond%000.png"
+        self.ssfnameformat = u"ScreenShot/[%yado%/[%party%_]]%year%%month%%day%_%hour%%minute%%second%[_in_%scenario%].png"
+        self.cardssfnameformat = u"ScreenShot/[%yado%/[%party%_]]%year%%month%%day%_%hour%%minute%%second%[_in_%scenario%].png"
         self.titleformat = u"%application% %skin%[ - %yado%[ %scenario%]]"
         self.ssinfofontcolor = (0, 0, 0, 255)
         self.ssinfobackcolor = (255, 255, 255, 255)
