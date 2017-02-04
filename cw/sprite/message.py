9d4957f763b443592650f0800481ed251d8b6d4b
k4nagatsuki <none@none>
2017-02-05 03:26:12 +0900

    依然メッセージが欠ける

--- cw/sprite/message.py
+++ cw/sprite/message.py
@@ -225,11 +225,10 @@ class MessageWindow(base.CWPySprite):
             return
 
         if not self._fore:
-            yp_noscale = 9
-            lineheight_noscale = 22
-            bottom = self.rect_noscale[3]-yp_noscale-lineheight_noscale*7
-            h = max(self.bottom_noscale+bottom, self.blockbottom_noscale) - min(self.top_noscale-9, self.blocktop_noscale)
-            h += self.trim_top_noscale
+            if self.centering_y:
+                h = self.blockbottom_noscale-self.blocktop_noscale
+            else:
+                h = self.rect_noscale[3]
             self._fore = pygame.Surface(cw.s((470, h))).convert_alpha()
             self._fore.fill((0, 0, 0, 0))
             self._back = self._fore.copy()
