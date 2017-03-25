17b751d2982b5567bcae01ec6087175c26653fc4
k4nagatsuki <none@none>
2017-03-25 23:57:21 +0900

    不要なコピーが発生する

--- cw/data.py
+++ cw/data.py
@@ -494,7 +494,6 @@ class SystemData(object):
             data = self.get_beastdata(resid)
             if data is None:
                 return
-            data = copy.deepcopy(data)
             data = xml2etree(element=data)
             dstpath = cw.util.relpath(path, self.tempdir)
         else:
@@ -504,11 +503,12 @@ class SystemData(object):
             except:
                 cw.util.print_ex()
                 return
-            data = copy.deepcopy(data)
             dstpath = cw.util.relpath(path, scedir)
 
         if path in imgpaths:
             return
+
+        data = copy.deepcopy(data)
 
         dstpath = cw.util.join_paths(dstdir, dstpath)
         imgpaths[path] = dstpath
