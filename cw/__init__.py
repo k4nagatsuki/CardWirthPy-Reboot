44eeca4cecc14be18c8d3791b3bd2dcfe872cc90
k4nagatsuki <none@none>
2017-06-01 19:04:04 +0900

    2.0β2

--- cw/__init__.py
+++ cw/__init__.py
@@ -50,7 +50,7 @@ tempdir_init = u"Data/Temp/Global"
 tempdir = tempdir_init
 
 # アプリケーション情報
-APP_VERSION = (2, "0 Beta 1")
+APP_VERSION = (2, "0 Beta 2")
 APP_NAME = "CardWirthPy"
 
 # CardWirthの標準文字コード
