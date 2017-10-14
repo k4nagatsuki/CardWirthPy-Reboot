24484c56feee402ec514a71cd463d983923f4be4
k4nagatsuki <none@none>
2017-10-15 01:04:10 +0900

    設定ダイアログの適用ボタンが有効にならない issue #590

--- cw/frame.py
+++ cw/frame.py
@@ -1291,6 +1291,7 @@ class MyApp(wx.App):
         self.SetAppName(cw.APP_NAME)
         self.SetVendorName("")
         self.call_filterevent = False
+        self.SetCallFilterEvent(True)
         skincount = get_skincount()[0]
         exe = u""
         if len(cw.SKIN_CONV_ARGS) > 0 and cw.SKIN_CONV_ARGS[0].lower().endswith(".exe"):
@@ -1328,15 +1329,15 @@ class MyApp(wx.App):
         if not event:
             return -1
 
-        if self.call_filterevent:
-            return -1
-
         if cw.cwpy.frame.filter_event:
             if not event.GetEventObject():
                 return -1
 
             if cw.cwpy.frame.filter_event(event):
                 return True
+
+        if self.call_filterevent:
+            return -1
 
         if not isinstance(event, wx.KeyEvent):
             return -1
