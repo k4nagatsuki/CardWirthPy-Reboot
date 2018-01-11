10806e8fe9c28bc4ae9fb7b9127dd8ffcbfa8fe9
tachi_gigas <tachi_gigas@yahoo.co.jp>
2018-01-11 20:55:47 +0900

    32レベルを超えるNPCを連れ込む時に正しく経験点が入らない

--- cw/character.py
+++ cw/character.py
@@ -1948,7 +1948,7 @@ class Character(object):
         if cw.cwpy.ydata:
             cw.cwpy.ydata.changed()
         value = int(value)
-        value = cw.util.numwrap(value, -999, 999)
+        value = cw.util.numwrap(value, -9999, 9999)
         removed = self._remove_coupon(name, False)
         e = self.data.make_element("Coupon", name, {"value" : str(value)})
         self.data.append("Property/Coupons", e)
