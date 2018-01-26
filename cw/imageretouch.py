248c8e276c3f4cf874e5a23864aaabbbce6854c6
k4nagatsuki <none@none>
2018-01-26 20:45:20 +0900

    Windows 10 1709で「游明朝」を使用して「ーム」を含む文字列を描画するとエラーになる事がある問題を回避 issue #623

--- cw/imageretouch.py
+++ cw/imageretouch.py
@@ -1162,6 +1162,9 @@ class Font(object):
             text = text.encode("utf-8")
             size = _imageretouch.font_imagesize(self.fontinfo2x, text, antialias)
             buf = _imageretouch.font_render(self.fontinfo2x, text, antialias, colour[:3])
+            # BUG: Windows 10 1709で、フォント「游明朝」で「ーム」を含む文字列のサイズが
+            #      ランダムに変動してしまう不具合を暫定的に回避する
+            size = (len(buf) // size[1] // 4, size[1])
             assert len(buf) == size[0]*size[1]*4
             image = pygame.image.frombuffer(buf, size, "RGBA").convert_alpha()
             size2 = _imageretouch.font_imagesize(self.fontinfo, text, antialias)
@@ -1174,6 +1177,9 @@ class Font(object):
 #            buf, size = _imageretouch.font_render(self.fontinfo, str, antialias, colour[:3])
             size = _imageretouch.font_imagesize(self.fontinfo, text, antialias)
             buf = _imageretouch.font_render(self.fontinfo, text, antialias, colour[:3])
+            # BUG: Windows 10 1709で、フォント「游明朝」で「ーム」を含む文字列のサイズが
+            #      ランダムに変動してしまう不具合を暫定的に回避する
+            size = (len(buf) // size[1] // 4, size[1])
             assert len(buf) == size[0]*size[1]*4
             bmp = pygame.image.frombuffer(buf, size, "RGBA").convert_alpha()
 
