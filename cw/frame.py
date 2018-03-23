bb50eccae1c4e7a8eed370d3d981ca4ae40be98e
k4nagatsuki <none@none>
2018-03-24 00:23:38 +0900

    シナリオ選択ダイアログをDestroyする

--- cw/frame.py
+++ cw/frame.py
@@ -746,8 +746,11 @@ class Frame(wx.Frame):
         # FIXME: linuxでたまに操作不能になる
         #        Windowsでも環境によって落ちる事がある
         #        kill_dlgを遅延させる事で問題を回避する
-        self.kill_dlg(None)
-        self.append_killlist(dlg)
+        #self.kill_dlg(None)
+        #self.append_killlist(dlg)
+
+        # wxPython 4.0.2にアップデートしたのでで様子見
+        self.kill_dlg(dlg)
 
     @synclock(_killlist_mutex)
     def append_killlist(self, dlg):
