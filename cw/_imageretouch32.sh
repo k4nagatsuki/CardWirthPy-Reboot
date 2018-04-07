146ef996ebcb5990c649b795f12b025cacd31549
k4nagatsuki <none@none>
2018-04-07 21:59:24 +0900

    linux 32

--- cw/_imageretouch32.sh
+++ cw/_imageretouch32.sh
@@ -1,3 +1,3 @@
 set -x
-gcc -shared _imageretouch.c -o_imageretouch32.so -I/usr/include/python3.5 -L/usr/include/python3.5 -fPIC -m32 -O3 -Wall
+gcc -shared _imageretouch.c -o_imageretouch32.so -I/usr/include/python3.4 -L/usr/include/python3.4 -fPIC -m32 -O3 -Wall
 
