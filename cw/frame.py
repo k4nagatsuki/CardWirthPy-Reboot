e716fb3710a38756ac3d4ea23439b07f489aabcb
k4nagatsuki <none@none>
2018-04-08 19:59:19 +0900

    linuxでの起動前にウィンドウサイズ等を設定しておく

--- cw/frame.py
+++ cw/frame.py
@@ -54,14 +54,60 @@ class Frame(wx.Frame):
         self.thread = threading.currentThread()
         self._skindirname = skindirname
 
+        def set_bounds():
+            setfullscreensize = False
+            if sys.platform != "win32":
+                if self._setting.is_expanded and self._setting.expandmode == "FullScreen":
+                    setfullscreensize = True
+                else:
+                    self.SetClientSize(cw.wins(cw.SIZE_GAME))
+
+                if not setfullscreensize:
+                    self.SetMinSize(self.GetBestSize())
+                    self.SetMaxSize(self.GetBestSize())
+
+            def adjust_position():
+                if not (self._setting.window_position[0] is None and self._setting.window_position[1] is None):
+                    pos = self.GetPosition()
+                    if not self._setting.window_position[0] is None:
+                        pos = (self._setting.window_position[0], pos[1])
+                    if not self._setting.window_position[1] is None:
+                        pos = (pos[0], self._setting.window_position[1])
+                    self.SetPosition(pos)
+                    cw.util.adjust_position(self)
+
+            adjust_position()
+
+            if setfullscreensize:
+                self.SetClientSize(self.get_displaysize())
+
+            # 拡大後のウィンドウがモニタに収まらない場合は縮小状態に戻す
+            d = wx.Display.GetFromWindow(self)
+            if d == wx.NOT_FOUND: d = 0
+            drect = wx.Display(d).GetClientArea()
+            wsize = self.GetBestSize()
+            if self._setting.is_expanded and (drect[2] < wsize[0] or drect[3] < wsize[1]):
+                self._setting.is_expanded = False
+                cw.UP_WIN = 1
+                cw.UP_WIN_M = cw.UP_WIN
+                cw.UP_SCR = 1
+                self.SetClientSize(cw.wins(cw.SIZE_GAME))
+                if sys.platform != "win32":
+                    self.SetMinSize(self.GetBestSize())
+                    self.SetMaxSize(self.GetBestSize())
+                self.panel.SetSize(cw.wins(cw.SIZE_GAME))
+                adjust_position()
+
         if sys.platform == "win32":
             self.panel = wx.Panel(self, -1, size=cw.wins(cw.SIZE_GAME), style=wx.NO_BORDER)
+            set_bounds()
             self._start_wx()
         else:
             # Xではウィンドウが表示されるまでウィンドウハンドルが取れない
             wx.Frame.__init__(self, None, -1, cw.APP_NAME)
             self.panel = wx.Panel(self, -1, size=cw.wins(cw.SIZE_GAME), style=wx.NO_BORDER)
             self.panel.SetDoubleBuffered(False)
+            set_bounds()
             self.Show()
             self._retry_count = 0
             wx.CallLater(100, self._start_wx)
@@ -77,48 +123,6 @@ class Frame(wx.Frame):
                               style=wx.OK|wx.CENTRE|wx.ICON_ERROR, parent=self)
                 self.Destroy()
             return
-
-        setfullscreensize = False
-        if sys.platform != "win32":
-            if self._setting.is_expanded and self._setting.expandmode == "FullScreen":
-                setfullscreensize = True
-            else:
-                self.SetClientSize(cw.wins(cw.SIZE_GAME))
-
-        if sys.platform != "win32" and not setfullscreensize:
-            self.SetMinSize(self.GetBestSize())
-            self.SetMaxSize(self.GetBestSize())
-
-        def adjust_position():
-            if not (self._setting.window_position[0] is None and self._setting.window_position[1] is None):
-                pos = self.GetPosition()
-                if not self._setting.window_position[0] is None:
-                    pos = (self._setting.window_position[0], pos[1])
-                if not self._setting.window_position[1] is None:
-                    pos = (pos[0], self._setting.window_position[1])
-                self.SetPosition(pos)
-                cw.util.adjust_position(self)
-
-        adjust_position()
-        if setfullscreensize:
-            self.SetClientSize(self.get_displaysize())
-
-        # 拡大後のウィンドウがモニタに収まらない場合は縮小状態に戻す
-        d = wx.Display.GetFromWindow(self)
-        if d == wx.NOT_FOUND: d = 0
-        drect = wx.Display(d).GetClientArea()
-        wsize = self.GetBestSize()
-        if self._setting.is_expanded and (drect[2] < wsize[0] or drect[3] < wsize[1]):
-            self._setting.is_expanded = False
-            cw.UP_WIN = 1
-            cw.UP_WIN_M = cw.UP_WIN
-            cw.UP_SCR = 1
-            self.SetClientSize(cw.wins(cw.SIZE_GAME))
-            if sys.platform != "win32":
-                self.SetMinSize(self.GetBestSize())
-                self.SetMaxSize(self.GetBestSize())
-            self.panel.SetSize(cw.wins(cw.SIZE_GAME))
-            adjust_position()
 
         os.environ["SDL_WINDOWID"] = str(self.panel.GetHandle())
         if sys.platform == "win32":
