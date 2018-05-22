28cd6a77c3932b0043a41f51068ca3252b246fa9
k4nagatsuki <none@none>
2018-05-22 20:23:49 +0900

    JPY1でJPDCで撮影されたイメージへ設定した`alpha`が機能しない

--- cw/effectbooster.py
+++ cw/effectbooster.py
@@ -518,7 +518,7 @@ class _JpySubImage(cw.image.Image):
 
         # 透明度
         if self.paintmode == 3:
-            if image.get_flags() & pygame.locals.SRCALPHA and (not isinstance(image.get_alpha(), int) or image.get_alpha() == 255):
+            if image.get_flags() & pygame.locals.SRCALPHA:
                 image.fill((0, 0, 0, 255 - self.alpha), special_flags=pygame.locals.BLEND_RGBA_SUB)
             else:
                 image.set_alpha(self.alpha)
