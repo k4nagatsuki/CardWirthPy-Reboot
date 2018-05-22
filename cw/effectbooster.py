021f9a0f454bae6fee7bdc133f5776bf009765dd
k4nagatsuki <none@none>
2018-05-22 21:56:59 +0900

    Surface#get_bitsize()も当てにならない

--- cw/effectbooster.py
+++ cw/effectbooster.py
@@ -518,7 +518,8 @@ class _JpySubImage(cw.image.Image):
 
         # 透明度
         if self.paintmode == 3:
-            if 32 <= image.get_bitsize():
+            print(image.get_bitsize())
+            if 32 <= image.get_bitsize() and (image.get_flags() & pygame.locals.SRCALPHA):
                 image.fill((255, 255, 255, self.alpha), special_flags=pygame.locals.BLEND_RGBA_MULT)
             else:
                 image.set_alpha(self.alpha)
