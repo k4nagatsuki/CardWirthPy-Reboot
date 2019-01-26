93955e9cdf4d348ffd25e3854aeac45a8b9763e0
k4nagatsuki <none@none>
2019-01-26 19:04:43 +0900

    ランダム多岐分岐とカード再配置コンテントのアイコンをCWXEditorの上書きリソースからロードできない

--- cw/setting.py
+++ cw/setting.py
@@ -2667,7 +2667,7 @@ CWXEDITOR_RESOURCES = {
     "EVT_BRANCH_SELECT": "evt_br_member.png",  # メンバ選択
     "EVT_BRANCH_ABILITY": "evt_br_power.png",  # 能力判定分岐
     "EVT_BRANCH_RANDOM": "evt_br_random.png",  # ランダム分岐
-    "EVT_BRANCH_MULTI_RANDOM": "evt_br_multi_random.png",  # ランダム多岐分岐
+    "EVT_BRANCH_MULTIRANDOM": "evt_br_multi_random.png",  # ランダム多岐分岐
     "EVT_BRANCH_LEVEL": "evt_br_level.png",  # レベル判定分岐
     "EVT_BRANCH_STATUS": "evt_br_state.png",  # 状態判定分岐
     "EVT_BRANCH_PARTYNUMBER": "evt_br_num.png",  # 人数判定
@@ -2715,6 +2715,7 @@ CWXEDITOR_RESOURCES = {
     # Visual
     "EVT_SHOW_PARTY": "evt_show_party.png",  # パーティ表示
     "EVT_HIDE_PARTY": "evt_hide_party.png",  # パーティ隠蔽
+    "EVT_MOVE_CARD": "evt_mv_card.png",  # カード再配置
     "EVT_MOVE_BGIMAGE": "evt_mv_back.png",  # 背景再配置
     "EVT_REPLACE_BGIMAGE": "evt_rpl_back.png",  # 背景置換
     "EVT_LOSE_BGIMAGE": "evt_lose_back.png",  # 背景削除
