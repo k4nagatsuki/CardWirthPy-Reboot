9cb096cf11559dd73925c2c3ac832bc81cd50fa0
k4nagatsuki <none@none>
2019-01-29 21:36:20 +0900

    ダイアログ表示前に画面を更新する

--- cw/thread.py
+++ cw/thread.py
@@ -1686,6 +1686,7 @@ class CWPy(_Singleton, threading.Thread):
         self.input(eventclear=True)
         self._showingdlg += 1
         self.statusbar.clear_volumebar()
+        self.add_lazydraw(clip=pygame.Rect((0, 0), cw.SIZE_GAME))
         if self.selection and self.selection.is_statusctrl:
             # 表示が乱れる場合があるので
             # ステータスバーのボタンからフォーカスを外しておく
