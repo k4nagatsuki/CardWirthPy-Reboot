1340e951b26599371a18067c1da136c2d0fa0c04
k4nagatsuki <none@none>
2019-02-15 20:09:11 +0900

    ファイルをメイン画面へドロップしてからダイアログが表示されるまでの間に追加でドロップを行うと二重にドロップ処理が行われる

--- cw/frame.py
+++ cw/frame.py
@@ -516,6 +516,8 @@ class Frame(wx.Frame):
         cw.thread.post_pygameevent(evt)
 
     def OnDropFiles(self, event):
+        if cw.cwpy.is_showingdlg():
+            return
         paths = event.GetFiles()
 
         for path in paths:
