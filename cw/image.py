be914a1ba86124dc38dae2f7e0b3a395055b2fec
k4nagatsuki <none@none>
2019-03-31 14:35:35 +0900

    マージ

--- cw/image.py
+++ cw/image.py
@@ -1411,6 +1411,9 @@ def smoothscale(surface, size, smoothing=True, iscard=False):
         return surface
     size = [max(1, a) for a in size]
 
+    if surface.get_width() == 0 or surface.get_height() == 0:
+        return pygame.Surface(size).convert()
+
     if surface.get_height() <= 1:
         # FIXME: 環境によって、高さが1の画像に
         #        pygame.transform.smoothscale()を行うと
