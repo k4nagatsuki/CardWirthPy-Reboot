321ef570775f8bdc1c0b7cdde80222df5e2c418a
k4nagatsuki <none@none>
2019-04-19 23:26:35 +0900

    JPDCイメージを背景セルとして直接配置した時にマスク指定が機能しない issue #787

--- cw/effectbooster.py
+++ cw/effectbooster.py
@@ -977,9 +977,6 @@ class JpdcImage(cw.image.Image):
             if rect2.colliderect(rect):
                 self.image.blit(image.subsurface(rect2.clip(rect)), cw.s((0, 0)))
 
-        if mask:
-            self.image.set_colorkey(self.image.get_at((0, 0)), pygame.locals.RLEACCEL)
-
         # 画像保存
         filename = config.get("jpdc:init", "savefilename", "")
         savecomment = config.get("jpdc:init", "savecomment", "")
@@ -1071,6 +1068,10 @@ class JpdcImage(cw.image.Image):
                 cw.cwpy.update_titlebar()
 
             cw.cwpy.background.reload_jpdcimage = False
+
+        if mask:
+            self.image = self.image.convert()
+            self.image.set_colorkey(self.image.get_at((0, 0)), pygame.locals.RLEACCEL)
 
     def wait(self, doanime):
         # 右クリックするまで待機
