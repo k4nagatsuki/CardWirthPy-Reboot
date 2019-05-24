a317e8d0134c67f30e9deea9173864e3283f2ecd
k4nagatsuki <none@none>
2019-05-24 23:47:57 +0900

    メッセージの表示中にメッセージ関係のオプションを変更すると、「高さを圧縮」以外のメッセージログ及びプレイログで選択肢が増殖する

--- cw/sprite/message.py
+++ cw/sprite/message.py
@@ -309,6 +309,7 @@ class MessageWindow(base.CWPySprite):
             cw.cwpy.list = self.selections
         x_noscale, y_noscale = self.rect_noscale.left, self.rect_noscale.bottom
 
+        self.names_log = []
         for index, name in enumerate(self.names):
             # 互換動作: 1.30以前は選択肢に特殊文字を使用しない
             if not self.backlog and self._barspchr and not cw.cwpy.sct.lessthan("1.30", cw.cwpy.sdata.get_versionhint(cw.HINT_CARD)):
