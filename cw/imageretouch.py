2ad0d7b496df7bcb5a599989feac66b9974a62cf
k4nagatsuki <none@none>
2019-05-25 11:20:36 +0900

    縦書き用フォントの代替フォントの探索が行われない

--- cw/imageretouch.py
+++ cw/imageretouch.py
@@ -1317,6 +1317,8 @@ def get_fontface(fontface):
     差し替え用のフォント名を返す。
     存在するフォントであればfontfaceを返す。
     """
+    if fontface.startswith('@'):
+        return '@' + get_fontface(fontface[1:])
     if not cw.cwpy.rsrc or fontface in cw.cwpy.rsrc.facenames:
         return fontface
 
