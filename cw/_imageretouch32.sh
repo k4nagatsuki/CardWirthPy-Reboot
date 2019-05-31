4f9e0ad8e19cc39f3db01736004e5b223a8fed9a
k4nagatsuki <none@none>
2019-05-31 23:37:53 +0900

    linux環境での問題の修正

--- cw/_imageretouch32.sh
+++ cw/_imageretouch32.sh
@@ -1,3 +1,3 @@
 set -x
-gcc -shared _imageretouch.c -o_imageretouch32.so -I/usr/include/python3.4 -L/usr/include/python3.4 -fPIC -m32 -O3 -Wall
+gcc -shared _imageretouch.c -o_imageretouch32.so -I/usr/include/python3.6 -L/usr/include/python3.6 -fPIC -m32 -O3 -Wall
 
--- cw/_imageretouch64.sh
+++ cw/_imageretouch64.sh
@@ -1,3 +1,3 @@
 set -x
-gcc -shared _imageretouch.c -o_imageretouch64.so -I/usr/include/python3.5 -L/usr/include/python3.5 -fPIC -m64 -O3 -Wall
+gcc -shared _imageretouch.c -o_imageretouch64.so -I/usr/include/python3.6 -L/usr/include/python3.6 -fPIC -m64 -O3 -Wall
 
