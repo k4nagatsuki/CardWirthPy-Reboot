82f8f2f8c576688c6b57991a8a5faba8fa514f95
k4nagatsuki <none@none>
2020-05-01 13:17:08 +0900

    ビルドスクリプトの更新

--- example_dailybuild.bat
+++ example_dailybuild.bat
@@ -11,9 +11,9 @@ rem CardWirthPyのローカルリポジトリ
 set LOCAL_REPO_ENGINE=D:\path\to\cardwirthpy-reboot
 
 rem Python(32-bit)
-set PYTHON_32="C:\Program Files (x86)\Python37-32\python.exe"
+set PYTHON_32="C:\Program Files (x86)\Python38-32\python.exe"
 rem Python(64-bit)
-set PYTHON_64="C:\Program Files\Python37\python.exe"
+set PYTHON_64="C:\Program Files\Python38\python.exe"
 
 rem CardWirthPyプレイヤーズガイドのローカルリポジトリのパス
 set CARDWIRTHPY_HELP_DIR=D:\path\to\CWPy_Help
--- example_rollback_dailybuild.bat
+++ example_rollback_dailybuild.bat
@@ -1,4 +1,4 @@
 rem 間違ってF9版を作った時に、アーカイブの末尾にある文字(a, b, c, ...)をロールバックします。
 
-"C:\Program Files\Python37\python.exe" rollback_dailybuild.py
-"C:\Program Files (x86)\Python37-32\python.exe" rollback_dailybuild.py
+"C:\Program Files\Python38\python.exe" rollback_dailybuild.py
+"C:\Program Files (x86)\Python38-32\python.exe" rollback_dailybuild.py
