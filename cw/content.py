896b4d40841f6f60eb589e8349875558492aed51
Tachi Gigas <tachi_gigas@yahoo.co.jp>
2020-12-19 22:44:58 +0900

    カードの効果対象はメニューカードも含まれる

--- cw/content.py
+++ cw/content.py
@@ -2497,7 +2497,7 @@ class EffectContent(EventContentBase):
 
     def action(self) -> int:
         """効果コンテント。"""
-        target: Optional[Union[cw.character.Character, List[cw.character.Character], cw.header.CardHeader]]
+        target: Optional[Union[cw.character.Character, List[Union[cw.character.Character, cw.sprite.card.CWPyCard]], cw.header.CardHeader]]
         if self.targetm == "CardTarget":
             # カードの使用対象(Wsn.2)
             target = []
@@ -2506,7 +2506,7 @@ class EffectContent(EventContentBase):
                 assert e_effectevent
                 e_effectevent.update_targets()
                 for card in e_effectevent.targets:
-                    assert isinstance(card, cw.character.Character)
+                    assert isinstance(card, cw.character.Character) or isinstance(card, cw.sprite.card.CWPyCard)
                     target.append(card)
         else:
             target = cw.cwpy.event.get_targetmember(self.targetm, coupon=self.holdingcoupon)
