9c3ca96aa8d8529bce8d91415937eaf3e48efcaa
k4nagatsuki <none@none>
2021-09-25 12:38:18 +0900

    CardWirthに合わせて、カードの使用対象が自動選択される時に上に配置された効果が有効な対象が優先的に選択されるようにした issue #976

--- cw/character.py
+++ cw/character.py
@@ -1456,6 +1456,14 @@ class Character(object):
                     bonus = b
 
         if bonus == -2147483647:
+            targets2: List[cw.sprite.card.CWPyCard] = []
+            for motion in motions:
+                # 先に配置された効果の対象を優先する
+                for targ in targets:
+                    if targ.is_effective(header, motion):
+                        targets2.append(targ)
+                if targets2:
+                    return 0, targets2
             return 0, targets
         return bonus, targets if header.allrange else maxbonustargs
 
